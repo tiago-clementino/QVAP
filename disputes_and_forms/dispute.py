2d3a52c2ebd965a50c099ba134b4d7ee2ec0109e
tiago-clementino <tiago.luks@gmail.com>
2019-07-20 17:13:03 -0300

    get_items method in dispute.py

--- disputes_and_forms/dispute.py
+++ disputes_and_forms/dispute.py
@@ -3,9 +3,12 @@
 
 class Dispute:
 
-    def __init__(self, id, first, second, third, forth):
+    def __init__(self, id, first, second, third, fourth):
         self._id = id
         self.first = first
         self.second = second
         self.third = third
-        self.forth = forth
+        self.fourth = forth
+
+    def get_items(self):
+        return [self.first,self.second,self.third,self.fourth]
