8b18baa67d2b5c46b519e2b7fb3de2621ed00417
tiago-clementino <tiago.luks@gmail.com>
2019-07-23 11:49:20 -0300

    Getters and setters on class Dispute (dispute.py file), desides tested get_items method, and __eq__ and __ne__ customized methods.

--- disputes_and_forms/dispute.py
+++ disputes_and_forms/dispute.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
-
-
 class Dispute:
 
     def __init__(self, id, first, second, third, fourth):
-        self._id = id
+        self.id = id
         self.first = first
         self.second = second
         self.third = third
-        self.fourth = forth
+        self.fourth = fourth
+
+    def __eq__(self, other):
+        """Override the default Equals behavior"""
+        other_items = other.get_items()
+        return self.first in other_items and self.second in other_items and self.third in other_items and self.fourth in other_items
+
+    def __ne__(self, other):
+        """Override the default Unequal behavior"""
+        other_items = other.get_items()
+        return not self.first in other_items or not self.second in other_items or not self.third in other_items or not self.fourth in other_items
+
 
     def get_items(self):
         return [self.first,self.second,self.third,self.fourth]
+
+    @property
+    def id(self):
+         return self._id
+
+    @id.setter
+    def id(self, value):
+         self._id = value
