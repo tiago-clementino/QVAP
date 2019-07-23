f92ed242acd828aecd7d54941b9a072777a87521
tiago-clementino <tiago.luks@gmail.com>
2019-07-23 11:46:07 -0300

    Getters and setter on class Item (item.py file), besides customized __str__, __ne__ and __eq__ methods

--- disputes_and_forms/item.py
+++ disputes_and_forms/item.py
@@ -1,12 +1,38 @@
 # -*- coding: utf-8 -*-
-
-
 class Item:
 
-    def __init__(self, id, copy, shape, material, surface, constitution):
-        self._id = id
+    def __init__(self, id, copy, shape, material, surface, color, constitution):
+        self.id = id
         self.copy = copy
         self.shape = shape
         self.material = material
         self.surface = surface
+        self.color = color
         self.constitution = constitution
+
+    def __eq__(self, other):
+        """Override the default Equals behavior"""
+        return self.id == other.id
+
+    def __ne__(self, other):
+        """Override the default Unequal behavior"""
+        return self.id != other.id
+
+    @property
+    def id(self):
+         return self._id
+
+    @id.setter
+    def id(self, value):
+         self._id = value
+
+    @property
+    def dispute(self):
+         return self._dispute
+
+    @dispute.setter
+    def dispute(self, value):
+         self._dispute = value
+
+    def __str__(self):
+        return str(self.id)
