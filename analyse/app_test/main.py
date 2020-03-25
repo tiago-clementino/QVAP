5c520217378f1ef94c4bf4b87d16c76af2c376ed
tiago-clementino <tiago.luks@gmail.com>
2020-03-25 12:15:17 -0300

    App intro 2

--- analyse/app_test/main.py
+++ analyse/app_test/main.py
@@ -1,4 +1,5 @@
-from kivy.app import App
+#!/usr/bin/env python3
+""" from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
@@ -58,7 +59,7 @@ class MainApp(App):
                 self.solution.text = new_text
         self.last_button = button_text
         self.last_was_operator = self.last_button in self.operators
-    
+
     def on_solution(self, instance):
         text = self.solution.text
         if text:
@@ -67,4 +68,53 @@ class MainApp(App):
 
 if __name__ == "__main__":
     app = MainApp()
-    app.run()
+    app.run() """
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<MenuScreen>:
+    BoxLayout:
+        Button:
+            text: 'Goto settings'
+            on_press: 
+                root.manager.transition.direction = 'left'
+                root.manager.current = 'settings'
+        Button:
+            text: 'Quit'
+
+<SettingsScreen>:
+    BoxLayout:
+        Button:
+            text: 'My settings button'
+        Button:
+            text: 'Back to menu'
+            on_press: 
+                root.manager.transition.direction = 'right'
+                root.manager.current = 'menu'
+""")
+
+# Declare both screens
+class MenuScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
+
+class TestApp(App):
+
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    TestApp().run()
